a64df185f3587223a6f0f850a86534b7e33d6cfc
Grom-999 <sonikx666@gmail.com>
2022-03-21 13:35:57 +0300

    -style=Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<string>
-int main(){
-	//коментарий
-	std::string name;
-	std::cin >> name;
-	std::cout<<"hello world from "<<name<<std::endl;
-	return 0;
+#include <iostream>
+#include <string>
+int
+main()
+{
+  // коментарий
+  std::string name;
+  std::cin >> name;
+  std::cout << "hello world from " << name << std::endl;
+  return 0;
 }
